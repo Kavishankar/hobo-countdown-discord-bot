eeba1825f65bf13d4d3d6dda35d16ac33da02aa6
Kaviss <kaviss@cisco.com>
2018-04-22 21:50:42 +0530

    Timezone fix

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client();
 function sendDate(bot){
     var t2 = new Date(config.YEAR, config.MONTH, config.DAY);
     var t1 = new Date();
-    var diff = parseInt(((t2-t1)/1000)-30*24*60*60);
+    var diff = parseInt(((t2-t1)/1000)- (30*24*60*60) - (2*60*60));
     if(diff>0)
         var new_name = diff + " secs";
     else if(diff == 0)
@@ -18,7 +18,7 @@ function sendDate(bot){
 
 bot.on("ready", () => {
     console.log("Logged in as "+bot.user.username);
-    bot.user.setActivity('Infinity Watch!', { type: 'PLAYING' });
+    bot.user.setActivity('Party Organiser!', { type: 'PLAYING' });
     setInterval(sendDate,1000,bot);
 
 });
