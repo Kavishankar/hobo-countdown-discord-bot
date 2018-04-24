e7a71fddde928e69795cb4c752513250017dd2be
Kaviss <kaviss@cisco.com>
2018-04-24 23:50:06 +0530

    Infinity countdown

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ const bot = new Discord.Client();
 function sendDate(bot){
     var t2 = new Date(config.YEAR, config.MONTH, config.DAY);
     var t1 = new Date();
-    var diff = parseInt(((t2-t1)/1000)- (30*24*60*60) - (2*60*60));
+    var diff = parseInt(((t2-t1)/1000)- (30*24*60*60));
     if(diff>0)
-        var new_name = diff + " secs";
+        var new_name = diff + "secs";
     else if(diff == 0)
         var new_name = config.MSG;
     else
